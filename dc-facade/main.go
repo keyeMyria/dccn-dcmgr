fc6a235aa91eb23236870e40c360b36a6cce0583
yousongzhang <yzhang303@dons.usfca.edu>
2019-04-10 23:12:10 -0700

    [SWDEV-182]add-exmaple

--- dc-facade/main.go
+++ dc-facade/main.go
@@ -1,17 +1,8 @@
 package main
 
 import (
-	"github.com/Ankr-network/dccn-common/protos/dcmgr/v1/micro"
-	"github.com/Ankr-network/dccn-dcmgr/dc-facade/handler"
-	"github.com/Ankr-network/dccn-dcmgr/dc-facade/subscriber"
-	"log"
-
-	"github.com/micro/go-micro"
-
-	"github.com/Ankr-network/dccn-common/protos"
 	"github.com/Ankr-network/dccn-dcmgr/dc-facade/config"
 	"github.com/Ankr-network/dccn-dcmgr/dc-facade/db-service"
-	"github.com/micro/go-grpc"
 	_ "github.com/micro/go-plugins/broker/rabbitmq"
 )
 
@@ -22,59 +13,59 @@ var (
 )
 
 func main() {
-	Init()
-
-	if db, err = dbservice.New(conf.DB); err != nil {
-		log.Fatal(err.Error())
-	}
-	defer db.Close()
-
-	startHandler()
+	//Init()
+	//
+	//if db, err = dbservice.New(conf.DB); err != nil {
+	//	log.Fatal(err.Error())
+	//}
+	//defer db.Close()
+	//
+	//startHandler()
 }
-
-// Init starts handler to listen.
-func Init() {
-	log.SetFlags(log.Lshortfile | log.LstdFlags)
-
-	if conf, err = config.Load(); err != nil {
-		log.Fatal(err.Error())
-	}
-	log.Printf("Load config %+v\n", conf)
-}
-
-func startHandler() {
-	srv := grpc.NewService(
-		micro.Name(ankr_default.DcMgrRegistryServerName),
-	)
-
-	// Initialise service
-	srv.Init()
-
-	// Register Task Handler
-
-	//Dc Manager register handler
-	//New Publisher to deploy new task action.
-	taskFeedback := micro.NewPublisher("FromDCFacadeToDCMgr", srv.Client())
-
-	dcHandler := handler.New(db, taskFeedback)
-
-	// Register Function as TaskStatusFeedback to update task by data center manager's feedback.
-	opt := srv.Server().Options()
-	opt.Broker.Connect()
-	if err := micro.RegisterSubscriber("dcMgrTaskDeploy", srv.Server(), subscriber.New(dcHandler.DcStreamCaches)); err != nil {
-		log.Fatal(err.Error())
-	}
-
-	// Register Dc Manager Handler
-	if err := dcmgr.RegisterDCStreamerHandler(srv.Server(), dcHandler); err != nil {
-		log.Fatal(err.Error())
-	}
-
-
-	//defer dcHandler.Cleanup()
-
-	// Run srv
-	if err := srv.Run(); err != nil {
-		log.Println(err.Error())
-	}
-}
+//
+//// Init starts handler to listen.
+//func Init() {
+//	log.SetFlags(log.Lshortfile | log.LstdFlags)
+//
+//	if conf, err = config.Load(); err != nil {
+//		log.Fatal(err.Error())
+//	}
+//	log.Printf("Load config %+v\n", conf)
+//}
+//
+//func startHandler() {
+//	srv := grpc.NewService(
+//		micro.Name(ankr_default.DcMgrRegistryServerName),
+//	)
+//
+//	// Initialise service
+//	srv.Init()
+//
+//	// Register Task Handler
+//
+//	//Dc Manager register handler
+//	//New Publisher to deploy new task action.
+//	taskFeedback := micro.NewPublisher("FromDCFacadeToDCMgr", srv.Client())
+//
+//	dcHandler := handler.New(db, taskFeedback)
+//
+//	// Register Function as TaskStatusFeedback to update task by data center manager's feedback.
+//	opt := srv.Server().Options()
+//	opt.Broker.Connect()
+//	if err := micro.RegisterSubscriber("dcMgrTaskDeploy", srv.Server(), subscriber.New(dcHandler.DcStreamCaches)); err != nil {
+//		log.Fatal(err.Error())
+//	}
+//
+//	// Register Dc Manager Handler
+//	if err := dcmgr.RegisterDCStreamerHandler(srv.Server(), dcHandler); err != nil {
+//		log.Fatal(err.Error())
+//	}
+//
+//
+//	//defer dcHandler.Cleanup()
+//
+//	// Run srv
+//	if err := srv.Run(); err != nil {
+//		log.Println(err.Error())
+//	}
+//}
--- dockerfile/dc.Dockerfile
+++ dockerfile/dc.Dockerfile
@@ -5,5 +5,12 @@ COPY . .
 
 RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o cmd/dcmgr dcmgr/main.go
 
+FROM golang:1.11.4-alpine
 
-CMD ["cmd/dcmgr"]
+COPY . .
+
+COPY --from=builder /go/src/github.com/Ankr-network/dccn-dcmgr/cmd/dcmgr /
+
+
+
+CMD ["/dcmgr"]
