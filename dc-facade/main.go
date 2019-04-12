a82bc34c11ac5d8fe1f62639587e88364f1c12af
wweir <wweir@foxmail.com>
2019-04-12 11:39:01 +0800

    [SWDEV-122] update protobuf

--- dc-facade/main.go
+++ dc-facade/main.go
@@ -11,6 +11,7 @@ import (
 	_ "github.com/micro/go-plugins/broker/rabbitmq"
 )
 
+// FIXME: delete if not used
 var conf config.Config
 
 func init() {
@@ -25,7 +26,7 @@ func init() {
 
 func main() {
 	// connect to mongo
-	db, err := dbservice.New(conf.DB)
+	db, err := dbservice.New()
 	if err != nil {
 		//	log.Fatalln(err)
 	}
@@ -55,50 +56,3 @@ func main() {
 	//	log.Println(err.Error())
 	//}
 }
-//
-//// Init starts handler to listen.
-//func Init() {
-//	log.SetFlags(log.Lshortfile | log.LstdFlags)
-//
-//	if conf, err = config.Load(); err != nil {
-//		log.Fatal(err.Error())
-//	}
-//	log.Printf("Load config %+v\n", conf)
-//}
-//
-//func startHandler() {
-//	srv := grpc.NewService(
-//		micro.Name(ankr_default.DcMgrRegistryServerName),
-//	)
-//
-//	// Initialise service
-//	srv.Init()
-//
-//	// Register Task Handler
-//
-//	//Dc Manager register handler
-//	//New Publisher to deploy new task action.
-//	taskFeedback := micro.NewPublisher("FromDCFacadeToDCMgr", srv.Client())
-//
-//	dcHandler := handler.New(db, taskFeedback)
-//
-//	// Register Function as TaskStatusFeedback to update task by data center manager's feedback.
-//	opt := srv.Server().Options()
-//	opt.Broker.Connect()
-//	if err := micro.RegisterSubscriber("dcMgrTaskDeploy", srv.Server(), subscriber.New(dcHandler.DcStreamCaches)); err != nil {
-//		log.Fatal(err.Error())
-//	}
-//
-//	// Register Dc Manager Handler
-//	if err := dcmgr.RegisterDCStreamerHandler(srv.Server(), dcHandler); err != nil {
-//		log.Fatal(err.Error())
-//	}
-//
-//
-//	//defer dcHandler.Cleanup()
-//
-//	// Run srv
-//	if err := srv.Run(); err != nil {
-//		log.Println(err.Error())
-//	}
-//}
--- dcmgr/subscriber/subscribe_dc.go
+++ dcmgr/subscriber/subscribe_dc.go
@@ -24,9 +24,9 @@ func (p *DCFacade) HandlerFeedBackFromDCFacade(req *common_proto.DCStream) error
 		if err := p.handler.UpdateDataCenter(in.GetDataCenter()); err != nil {
 			log.Println(err.Error())
 		}
-	case common_proto.DCOperation_TASK_CREATE,
-		common_proto.DCOperation_TASK_UPDATE,
-		common_proto.DCOperation_TASK_CANCEL: // update task status
+	case common_proto.DCOperation_APP_CREATE,
+		common_proto.DCOperation_APP_UPDATE,
+		common_proto.DCOperation_APP_CANCEL: // update task status
 		p.handler.UpdateTask(in)
 	default:
 		log.Println(ankr_default.ErrUnknown.Error())
