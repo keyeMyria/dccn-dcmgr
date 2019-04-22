9f4ada317978e6e515c7f4144d78b2dd14a28458
wweir <wweir@foxmail.com>
2019-04-22 11:30:42 +0800

    [SWDEV-122] add log for daemon disconnect

--- dc-facade/handler/hook.go
+++ dc-facade/handler/hook.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"log"
 	"net"
 
 	common_proto "github.com/Ankr-network/dccn-common/protos/common"
@@ -11,6 +12,8 @@ func (p *Relay) OnAccept(key string, conn net.Conn) error      { return nil }
 func (p *Relay) OnBuild(key string, conn *yamux.Session) error { return nil }
 
 func (p *Relay) OnClose(key string, conn *yamux.Session) {
+	log.Printf("public %s close message", key)
+
 	p.taskFeedback.Publish(&common_proto.DCStream{
 		OpType: common_proto.DCOperation_HEARTBEAT,
 		OpPayload: &common_proto.DCStream_DataCenter{
